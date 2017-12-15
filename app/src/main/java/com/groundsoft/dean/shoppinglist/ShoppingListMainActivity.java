b1a0ee7183dca265782c6ce8390c688f385a4f2e
alexandersgr <phd.deen@gmail.com>
2017-12-15 14:38:04 +0700

    first

---
+++ app/src/main/java/com/groundsoft/dean/shoppinglist/ShoppingListMainActivity.java
@@ -0,0 +1,95 @@
+package com.groundsoft.dean.shoppinglist;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class ShoppingListMainActivity extends AppCompatActivity {
+
+    String[] name = { "Иван", "Марья", "Петр", "Антон", "Даша", "Борис",
+            "Костя", "Игорь" };
+    String[] position = { "Программер", "Бухгалтер", "Программер",
+            "Программер", "Бухгалтер", "Директор", "Программер", "Охранник" };
+    int salary[] = { 13000, 10000, 13000, 13000, 10000, 15000, 13000, 8000 };
+
+    int[] colors = new int[2];
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_list_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+/*
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+*/
+
+        colors[0] = Color.parseColor("#559966CC");
+        colors[1] = Color.parseColor("#55336699");
+
+        LinearLayout linLayout = (LinearLayout) findViewById(R.id.mainlist);
+
+
+
+        LayoutInflater ltInflater = getLayoutInflater();
+
+        for (int i = 0; i < name.length; i++) {
+            Log.d("myLogs", "i = " + i);
+            View item = ltInflater.inflate(R.layout.list_item, linLayout, false);
+            TextView tvName = (TextView) item.findViewById(R.id.tvName);
+            tvName.setText(name[i]);
+            TextView tvPosition = (TextView) item.findViewById(R.id.tvPosition);
+            tvPosition.setText("Должность: " + position[i]);
+            TextView tvSalary = (TextView) item.findViewById(R.id.tvSalary);
+            tvSalary.setText("Оклад: " + String.valueOf(salary[i]));
+            item.getLayoutParams().width = LinearLayout.LayoutParams.MATCH_PARENT;
+            item.setBackgroundColor(colors[i % 2]);
+            linLayout.addView(item);
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_shopping_list_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
